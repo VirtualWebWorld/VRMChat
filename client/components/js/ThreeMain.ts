7ae912fd693f8b6218955acdb5871c53e1e408c0
akatsuki1910 <akatsukicirno@gmail.com>
2021-03-13 18:58:26 +0900

    Update ThreeMain.ts

--- client/components/js/ThreeMain.ts
+++ client/components/js/ThreeMain.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import Stats from 'three/examples/jsm/libs/stats.module'
+// import Stats from 'three/examples/jsm/libs/stats.module'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export default class ThreeMain {
